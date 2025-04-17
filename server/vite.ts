4afc2ca8960e916675d532602f1eca7f9b641dab
Jhooper96 <hooperjeffrey44@gmail.com>
2025-04-17 12:20:20 -0400

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 import { createServer as createViteServer, createLogger } from "vite";
 import { type Server } from "http";
-import viteConfig from "../vite.config";
+import config from "../vite";
 import { nanoid } from "nanoid";
 
 const viteLogger = createLogger();
