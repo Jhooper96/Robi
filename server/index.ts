79c55190daa6508031b650861475075c6dc52c3e
Jhooper96 <hooperjeffrey44@gmail.com>
2025-04-17 14:10:26 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -5,14 +5,10 @@ app.use(express.json());
 import path from "path";
 import { registerRoutes } from "./routes";
 import { setupSMSRoutes } from "./routes";
-
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 // Serve static files from the public directory
 app.use(express.static(path.join(process.cwd(), 'public')));
-
 app.use((req, res, next) => {
   const start = Date.now();
   const path = req.path;
