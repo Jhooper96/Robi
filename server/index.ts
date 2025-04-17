f9cff5eac47ca987396c620843196d5e9e5be1a2
Jhooper96 <hooperjeffrey44@gmail.com>
2025-04-17 12:39:26 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -56,7 +56,6 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    serveStatic(app);
   }
 
   // ALWAYS serve the app on port 5000
